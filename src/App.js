c5d9d1e73088a78e7c9172139ed61426ae0e4565
ThanhDuong287 <thanhduong28720@gmail.com>
2022-07-04 23:20:15 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoItem from './components/ToDoItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ToDoItem />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ToDoItem extends Component {
+    render() {
+        return (
+            <div className="ToDoItem">
+                <h1>
+                    Có đi chợ không nào?
+                </h1>
+                <p>
+                    Đi chợ đi cưng
+                </p>
+            </div>
+        );
+    }
+}
+export default ToDoItem;
